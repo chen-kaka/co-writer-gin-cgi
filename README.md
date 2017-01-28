3acb1db1daa372ab07f732c653bbb0ce830b2de5
kakachan <chen-kaka@163.com>
2017-01-28 21:28:47 +0800

    gin init learning

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # go-gin
+
+    my project using go gin framework.
+
+- 1、安装 gin:
+
+go get gopkg.in/gin-gonic/gin.v1
+
+- 2、发现go get服务被墙, 配置lantern:
+
+查看进程监听端口：
+
+lsof -Pn | grep lantern | grep LISTEN
+
+配置蓝灯代理：
+
+在 ~/.bash_profile中添加:
+
+    export http_proxy=http://127.0.0.1:55945
+
+    export https_proxy=$http_proxy
+
+    export ftp_proxy=$http_proxy
+
+    export rsync_proxy=$http_proxy
+
+    export no_proxy="localhost,127.0.0.1,.dade.com"
+
+source ~/.bash_profile
+
+重新安装 go get gopkg.in/gin-gonic/gin.v1
+
+
---
+++ main/app.go
@@ -0,0 +1,76 @@
+package main
+
+import "gopkg.in/gin-gonic/gin.v1"
+import (
+	"net/http"
+	"fmt"
+	"os"
+	"log"
+	"io"
+)
+
+func main()  {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// However, this one will match /user/john/ and also /user/john/send
+	// If no other routers match /user/john, it will redirect to /user/john/
+	router.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
+
+	// Query string parameters are parsed using the existing underlying request object.
+	// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
+	router.GET("/welcome", func(c *gin.Context) {
+		firstname := c.DefaultQuery("firstname", "Guest")
+		lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+
+		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+	})
+
+	router.POST("/form_post", func(c *gin.Context) {
+		message := c.PostForm("message")
+		nick := c.DefaultPostForm("nick", "anonymous")
+
+		c.JSON(200, gin.H{
+			"status":  "posted",
+			"message": message,
+			"nick":    nick,
+		})
+	})
+
+	router.POST("/post", func(c *gin.Context) {
+
+		id := c.Query("id")
+		page := c.DefaultQuery("page", "0")
+		name := c.PostForm("name")
+		message := c.PostForm("message")
+
+		fmt.Printf("id: %s; page: %s; name: %s; message: %s", id, page, name, message)
+	})
+
+	router.POST("/upload", func(c *gin.Context) {
+
+		file, header , err := c.Request.FormFile("upload")
+		filename := header.Filename
+		fmt.Println(header.Filename)
+		out, err := os.Create("./tmp/"+filename+".png")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+		_, err = io.Copy(out, file)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	router.Run() // listen and serve on 0.0.0.0:8080
+}
